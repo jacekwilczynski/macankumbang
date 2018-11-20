e1b509210551533c7bbea47aff78d9878e301e7b
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-11-20 20:31:46 +0100

    Extract webpack config generator into a function

---
+++ getWebpackConfig.js
@@ -0,0 +1,56 @@
+// Utilities
+const path = require('path');
+
+// Webpack plugins
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+// The webpack config generator
+module.exports = function getWebpackConfig({
+  entry = './src/index.js',
+  devMode,
+  outputPath = 'dist'
+} = {}) {
+  return {
+    entry,
+    output: outputPath && { path: path.resolve(outputPath) },
+    mode: devMode ? 'development' : 'production',
+    devtool: devMode && 'cheap-module-eval-source-map',
+    plugins: [
+      new CleanWebpackPlugin([outputPath]),
+      new MiniCssExtractPlugin({
+        filename: hashCss ? '[name].[hash].css' : '[name].css',
+        chunkFilename: hashCss ? '[id].[hash].css' : '[id].css'
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: { presets: ['@babel/preset-env'] }
+          }
+        },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: { importLoaders: 2 }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: [require('autoprefixer')(), require('cssnano')()]
+              }
+            },
+            'sass-loader'
+          ]
+        }
+      ]
+    }
+  };
+};
